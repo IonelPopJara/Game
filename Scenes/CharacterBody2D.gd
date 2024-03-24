98c6f043572c56c4c2784f40939d3d93fe0dd718
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-03-24 11:51:49 +0100

    Tweak character controller again

--- Scenes/CharacterBody2D.gd
+++ Scenes/CharacterBody2D.gd
@@ -2,9 +2,17 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const JUMP_VELOCITY = -1000.0
+const MAX_JUMP_DELAY = 0.2
+const MAX_COYOTE_DELAY = 0.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*2
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 2
+
+var elapsedTimeSinceJump = 0.0
+var elapsedTimeSinceLastTouchedFloor = 0.0
+
+func _init():
+	elapsedTimeSinceJump = MAX_JUMP_DELAY
 
 func _process(delta):
 	if Input.is_action_pressed("fire"):
@@ -13,13 +21,21 @@ func _process(delta):
 		$AnimationPlayer.play("idle")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	elapsedTimeSinceJump += delta
+	
+	applyGravity(delta)
 
-	# Handle jump (only if the character is on the floor!)
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handle jump input
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or (!is_on_floor() and elapsedTimeSinceLastTouchedFloor < MAX_COYOTE_DELAY):
+			jump()
+		else:
+			elapsedTimeSinceJump = 0.0
+	
+	# If the last time the player pressed the jump button is less than the maximum window allowed,
+	# And the character is on the floor, jump
+	if elapsedTimeSinceJump <= MAX_JUMP_DELAY and is_on_floor():
+		jump()
 		
 	# Flip the sprite based on the direction
 	if Input.is_action_pressed("move_left"):
@@ -33,6 +49,23 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 5)
 
 	move_and_slide()
+
+func applyGravity(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		elapsedTimeSinceLastTouchedFloor += delta
+		# If the player holds the jump button, it falls slower
+		if Input.is_action_pressed("jump"):
+			velocity.y += gravity * delta
+		else:
+		# Otherwise, if the player doesn't hold the jump button, it falls faster
+			velocity.y += gravity * delta * 2
+	else:
+		elapsedTimeSinceLastTouchedFloor = 0.0
+
+func jump():
+	elapsedTimeSinceJump = MAX_JUMP_DELAY
+	velocity.y = JUMP_VELOCITY	
