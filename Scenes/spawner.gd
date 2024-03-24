8206748120fef1cdc26455685641cff17e6c1b25
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-03-24 17:01:17 +0100

    Fix clouds again

--- Scenes/spawner.gd
+++ Scenes/spawner.gd
@@ -7,6 +7,12 @@ var spawn_interval = 5  # Adjust this to change spawn interval
 
 var currentTime = 0.0
 
+func _init():
+	currentTime = 0.0
+	var cloud_instance = cloud_scene_path.instantiate()
+	cloud_instance.position = Vector2(0, randf_range(-500, 500))  # Random Y position
+	add_child(cloud_instance)
+
 func _process(delta):
 	currentTime += delta
 	
